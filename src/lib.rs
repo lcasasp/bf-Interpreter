7cbdfe27d419a6892be65c1d4fa82c222cc1db86
Lucas Casas <lucascasasp@icloud.com>
2023-12-20 22:59:58 -0700

    Implemented input/output and loop functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 //Bf uses an array of 30000 1 byte memory blocks
 const TAPE_SIZE: usize = 30000;
 
@@ -8,7 +6,6 @@ pub struct BfInterpreter {
     tape: [u8; TAPE_SIZE],
     // The data pointer
     pointer: usize,
-    // Other fields as needed (e.g., for loop handling)
 }
 
 impl BfInterpreter {
@@ -20,7 +17,34 @@ impl BfInterpreter {
     }
 
     pub fn execute(&mut self, program: &str) {
-       1;
+        let mut program_counter = 0;
+        let program_length = program.len();
+        let program_chars: Vec<char> = program.chars().collect();
+
+        while program_counter < program_length {
+            match program_chars[program_counter] {
+                '>' => self.move_right(),
+                '<' => self.move_left(),
+                '+' => self.increment(),
+                '-' => self.decrement(),
+                '.' => self.output(),
+                ',' => self.input(),
+                '[' => {
+                    if self.tape[self.pointer] == 0 {
+                        program_counter = self.jump_forward(program_counter, &program_chars);
+                        continue;
+                    }
+                }
+                ']' => {
+                    if self.tape[self.pointer] != 0 {
+                        program_counter = self.jump_backward(program_counter, &program_chars);
+                        continue;
+                    }
+                }
+                _ => {} // non-command characters are ignored as comments
+            }
+            program_counter += 1;
+        }
     }
 
     pub fn reset(&mut self) {
@@ -30,17 +54,17 @@ impl BfInterpreter {
 }
 
 impl BfInterpreter {
-    // Function to handle the '>' command
+    //handle the '>' command
     fn move_right(&mut self) {
         self.pointer += 1;
     }
 
-    // Function to handle the '<' command
+    //handle the '<' command
     fn move_left(&mut self) {
         self.pointer -= 1;
     }
 
-    // Function to handle the '+' command
+    //handle the '+' command
     fn increment(&mut self) {
         if self.pointer < TAPE_SIZE {
             self.tape[self.pointer] = self.tape[self.pointer].wrapping_add(1);
@@ -49,7 +73,7 @@ impl BfInterpreter {
         }
     }
 
-    // Function to handle the '-' command
+    //handle the '-' command
     fn decrement(&mut self) {
         if self.pointer < TAPE_SIZE {
             self.tape[self.pointer] = self.tape[self.pointer].wrapping_sub(1);
@@ -58,6 +82,68 @@ impl BfInterpreter {
         }
     }
 
+    //handle the '.' command
+    fn output(&self) {
+        use std::io::{self, Write};
+
+        let output_char = self.tape[self.pointer] as char;
+        print!("{}", output_char);
+        io::stdout().flush().unwrap()
+    }
+
+    //handle the ',' command
+    fn input(&mut self) {
+        use std::io::{self, Read};
+
+        let mut input_byte: [u8; 1] = [0; 1];
+        if io::stdin().read_exact(&mut input_byte).is_ok() {
+            self.tape[self.pointer] = input_byte[0];
+        } else {
+            panic!("Error reading input");
+        }
+    }
+
+    // handle '[' and ']' commands for loops
+    fn jump_forward(&self, start: usize, program: &[char]) -> usize {
+        let mut counter = 1;
+        let mut i = start + 1;
+
+        while i < program.len() && counter > 0 {
+            match program[i] {
+                '[' => counter += 1,
+                ']' => counter -= 1,
+                _ => {}
+            }
+            i += 1;
+        }
+
+        if counter == 0 {
+            i 
+        } else {
+            start // Fallback: return original position
+        }
+    }
+
+    fn jump_backward(&self, start: usize, program: &[char]) -> usize {
+        if start == 0 {
+            return 0;
+        }
+
+        let mut counter = 1;
+        let mut i = start;
+
+        while i > 0 {
+            i -= 1;
+            match program[i] {
+                '[' if counter == 1 => return i + 1,
+                '[' => counter -= 1,
+                ']' => counter += 1,
+                _ => {}
+            }
+        }
+        start  // Fallback: return original position
+    }
+    
 }
 
 #[cfg(test)]
@@ -90,7 +176,7 @@ mod tests {
     }
 
     #[test]
-    fn test_deccrement() {
+    fn test_decrement() {
         let mut interpreter: BfInterpreter = BfInterpreter::new();
         
         //test decrementing
@@ -102,5 +188,23 @@ mod tests {
         interpreter.tape[interpreter.pointer] = 0;
         interpreter.decrement();
         assert_eq!(interpreter.tape[interpreter.pointer], 255);
+    }
+
+    #[test]
+    fn test_jump_forward() {
+        let interpreter: BfInterpreter = BfInterpreter::new();
+        let program: Vec<char> = vec!['[', '+', '-', ']'];
+        let jump_position: usize = interpreter.jump_forward(0, &program);
+
+        assert_eq!(jump_position, 4, "Should jump to the position after matching ']'");
+    }
+
+    #[test]
+    fn test_jump_backward() {
+        let interpreter: BfInterpreter = BfInterpreter::new();
+        let program: Vec<char> = vec!['[', '+', '-', ']'];
+        let jump_position: usize = interpreter.jump_backward(3, &program);
+
+        assert_eq!(jump_position, 1, "Should jump to the position after matching '['");
     }
 }
